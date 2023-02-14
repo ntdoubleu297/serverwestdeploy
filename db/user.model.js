11de5dabf3d306e7801feb10c7382f916de20465
Neil T. White <87203253+ntdoubleu297@users.noreply.github.com>
2023-02-14 15:30:29 -0500

    Add files via upload

---
+++ db/user.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const User = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    //unique: true,
+  },
+
+  password: {
+    type: String,
+    requried: true,
+}
+
+},
+{ collection: 'user-data'}
+)
+
+const model = mongoose.model('userData', User)
+
+module.exports = model
